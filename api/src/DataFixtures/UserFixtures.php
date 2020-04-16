508eff42d4bdc8ae27aec06bd5132cd4cc930acc
Rached <bch.rached@gmail.com>
2020-04-16 05:33:36 +0200

    basic setting api platform entity

--- api/src/DataFixtures/UserFixtures.php
+++ api/src/DataFixtures/UserFixtures.php
@@ -6,8 +6,6 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Entity\User;
-use App\Entity\Manager;
-
 
 class UserFixtures extends Fixture
 {
@@ -21,40 +19,26 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $this->loadUsers($manager);
-        $this->loadManagers($manager);
     }
 
     private function loadUsers(ObjectManager $manager): void
     {
         $user = new User();
-        $user->setPassword($this->passwordEncoder->encodePassword($user, "hellouser10$"));
-        $user->setEmail("adresse1@domainevirtuel.com");
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'hellouser10$'));
+        $user->setEmail('adresse1@domainevirtuel.com');
+        $user->setFirstName('John');
+        $user->setLastName('Doe');
         $user->setRoles(['ROLE_ADMIN']);
         $manager->persist($user);
 
         $user2 = new User();
-        $user2->setPassword($this->passwordEncoder->encodePassword($user2, "hellouser10$"));
-        $user2->setEmail("adresse2@domainevirtuel.com");
+        $user2->setPassword($this->passwordEncoder->encodePassword($user2, 'hellouser10$'));
+        $user2->setFirstName('Mike');
+        $user2->setLastName('Dupon');
+        $user2->setEmail('adresse2@domainevirtuel.com');
         $user2->setRoles(['ROLE_ADMIN']);
         $manager->persist($user2);
-        
-        $manager->flush();
-    }
 
-    private function loadManagers(ObjectManager $manager): void
-    {
-        $user = new Manager();
-        $user->setPassword($this->passwordEncoder->encodePassword($user, "hellouser10$"));
-        $user->setEmail("adresse3@domainevirtuel.com");
-        $user->setRoles(['ROLE_ADMIN']);
-        $manager->persist($user);
-
-        $user2 = new Manager();
-        $user2->setPassword($this->passwordEncoder->encodePassword($user2, "hellouser10$"));
-        $user2->setEmail("adresse4@domainevirtuel.com");
-        $user2->setRoles(['ROLE_ADMIN']);
-        $manager->persist($user2);
-        
         $manager->flush();
     }
 }
--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -4,13 +4,17 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
+ * @ApiResource
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User implements UserInterface
 {
     /**
+     * @ApiProperty(identifier=true)
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
@@ -32,6 +36,16 @@ class User implements UserInterface
      * @ORM\Column(type="string")
      */
     private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
 
     public function getId(): ?int
     {
@@ -109,5 +123,29 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
     }
 }
